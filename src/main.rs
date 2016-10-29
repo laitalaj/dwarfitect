699452c859f07a3b8b9d6913a706d624c4074fa9
laitalaj <julius.laitala@helsinki.fi>
2016-10-29 10:57:55 +0300

    Fixed main function, now it actually works

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use dwarfilib::io::ui::{get_input_loop, get_parsed_input_loop};
 
 /// A main-function that brings everything together under a text UI.
 /// # Panics
-/// Panics if unable to save for some reason
+/// Panics if unable to read or save for some reason
 fn main() { //TODO: Move all this to actual functions
 	println!("Input file name: ");
 	let input_file = get_input_loop();
@@ -18,12 +18,12 @@ fn main() { //TODO: Move all this to actual functions
 	println!("Generations: ");
 	let generations: usize = get_parsed_input_loop();
 	let mut rng = rand::thread_rng();
-	let bp = input::read(input_file);
+	let bp = input::read(String::from(input_file.trim()));
 	let (genes, targets) = bp.compile();
 	let result = breeding::breeder(genes, targets, pop_size, generations, &mut rng);
 	let layout = result.as_layout();
 	let matrix = layout.as_char_matrix();
-	match output::save_matrix(matrix, output_file) {
+	match output::save_matrix(matrix, String::from(output_file.trim())) {
 		Err(reason) => panic!("{:?}", reason),
 		Ok(_) => {}
 	};
